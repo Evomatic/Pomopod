1e45c4e4831b64336aec2606f76bf29200075c75
Evan Trujillo <evomatic@pop-os.localdomain>
2021-05-04 00:15:55 +0900

    currently working on timer

---
+++ src/components/Timer.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div id="timer">
+        {{this.rendertime}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'Timer',
+    props: ['rendertime']
+    
+}
+</script>
+<style lang="">
+    
+</style>
